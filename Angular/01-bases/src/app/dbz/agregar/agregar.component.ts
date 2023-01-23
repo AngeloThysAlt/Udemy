d6880df5a173f825a086eb775acb5f4cb2a96a7b
angelo.thys <thys.angelo@bosonit.com>
2023-01-23 13:52:14 +0000

    .

--- Angular/01-bases/src/app/dbz/agregar/agregar.component.ts
+++ Angular/01-bases/src/app/dbz/agregar/agregar.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-agregar',
@@ -13,15 +14,15 @@ export class AgregarComponent{
     poder: 0
   }
 
-  @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+  constructor( private DbzService: DbzService) {}
+
 
   agregar() {
     if ( this.nuevo.nombre.trim().length === 0 ) {
       return;
     }
 
-    console.log(this.nuevo);
-    this.onNuevoPersonaje.emit( this.nuevo );
+    this.DbzService.agregarPersonaje( this.nuevo );
 
     this.nuevo = {
       nombre: '',
