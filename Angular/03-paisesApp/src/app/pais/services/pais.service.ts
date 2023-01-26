56f46b2e3431582a45b96179e99053700b29b41b
angelo.thys <thys.angelo@bosonit.com>
2023-01-26 09:13:05 +0000

    .

---
+++ Angular/03-paisesApp/src/app/pais/services/pais.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  private apiUrl: string = 'https://restcountries.com/v3.1'
+
+  constructor( private http: HttpClient ) { }
+
+
+}
